88c440524a5a7ed9062b6c63a684cd30cf849599
Erik Alveflo <erik@alveflo.se>
2017-11-11 16:19:47 +0100

    Converts the code into an Arduino compatible class

--- src/MRF24J40.cpp
+++ src/MRF24J40.cpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2017 Erik Alveflo
  * Copyright (c) 2014 Michele Balistreri
  * Copyright (c) 2011 Alex Hornung
  *
@@ -21,215 +22,255 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-#include "config.h"
 #include "MRF24J40.h"
 
-#include <stdlib.h>
+#include <SPI.h>
 
-#define mrf24j40_spi_preamble() volatile uint8_t tmpIE = mrf24j40_get_ie(); mrf24j40_set_ie(0); mrf24j40_cs_pin(0);
-#define mrf24j40_spi_postamble() mrf24j40_cs_pin(1); mrf24j40_set_ie(tmpIE);
+#define spi_preamble() noInterrupts(); _set_cs_pin(0);
+#define spi_postamble() _set_cs_pin(1); interrupts();
 
-void _mrf24j40_write_long_addr(uint16_t addr, uint8_t write) {
-  mrf24j40_spi_write(((addr >> 3) & 0x7F) | 0x80);
-  mrf24j40_spi_write(((addr << 5) & 0xE0) | (write << 4));
+Mrf24j40::Mrf24j40(int cs_pin, int reset_pin, int wake_pin)
+  : _cs_pin(cs_pin)
+  , _reset_pin(reset_pin)
+  , _wake_pin(wake_pin)
+{
 }
 
-void _mrf24j40_write_short_addr(uint8_t addr, uint8_t write) {
-  mrf24j40_spi_write(((addr << 1) & 0x7E) | write);
+void Mrf24j40::_set_cs_pin(bool level) {
+  digitalWrite(_cs_pin, level);
 }
 
-uint8_t mrf24j40_read_long_ctrl_reg(uint16_t addr) {
-  mrf24j40_spi_preamble();
-  _mrf24j40_write_long_addr(addr, 0);
-  uint8_t value = mrf24j40_spi_read();
-  mrf24j40_spi_postamble();
+void Mrf24j40::_set_reset_pin(bool level) {
+  if (_reset_pin >= 0) {
+    digitalWrite(_reset_pin, level);
+  }
+}
+
+void Mrf24j40::_set_wake_pin(bool level) {
+  if (_wake_pin >= 0) {
+    digitalWrite(_wake_pin, level);
+  }
+}
+
+void Mrf24j40::_write_long_addr(uint16_t addr, uint8_t write) {
+  spi_write(((addr >> 3) & 0x7F) | 0x80);
+  spi_write(((addr << 5) & 0xE0) | (write << 4));
+}
+
+void Mrf24j40::_write_short_addr(uint8_t addr, uint8_t write) {
+  spi_write(((addr << 1) & 0x7E) | write);
+}
+
+uint8_t Mrf24j40::read_long_ctrl_reg(uint16_t addr) {
+  spi_preamble();
+  _write_long_addr(addr, 0);
+  uint8_t value = spi_read();
+  spi_postamble();
 
   return value;
 }
 
-uint8_t mrf24j40_read_short_ctrl_reg(uint8_t addr) {
-  mrf24j40_spi_preamble();
-  _mrf24j40_write_short_addr(addr, 0);
-  uint8_t value = mrf24j40_spi_read();
-  mrf24j40_spi_postamble();
+uint8_t Mrf24j40::read_short_ctrl_reg(uint8_t addr) {
+  spi_preamble();
+  _write_short_addr(addr, 0);
+  uint8_t value = spi_read();
+  spi_postamble();
   return value;
 }
 
-void mrf24j40_write_long_ctrl_reg(uint16_t addr, uint8_t value) {
-  mrf24j40_spi_preamble();
-  _mrf24j40_write_long_addr(addr, 1);
-  mrf24j40_spi_write(value);
-  mrf24j40_spi_postamble();
+void Mrf24j40::write_long_ctrl_reg(uint16_t addr, uint8_t value) {
+  spi_preamble();
+  _write_long_addr(addr, 1);
+  spi_write(value);
+  spi_postamble();
 }
 
-void mrf24j40_write_short_ctrl_reg(uint8_t addr, uint8_t value) {
-  mrf24j40_spi_preamble();
-  _mrf24j40_write_short_addr(addr, 1);
-  mrf24j40_spi_write(value);
-  mrf24j40_spi_postamble();
+void Mrf24j40::write_short_ctrl_reg(uint8_t addr, uint8_t value) {
+  spi_preamble();
+  _write_short_addr(addr, 1);
+  spi_write(value);
+  spi_postamble();
 }
 
-void mrf24j40_ie(void) {
-  mrf24j40_write_short_ctrl_reg(MRF24J40_INTCON, ~(TXNIE | RXIE | SECIE));
+void Mrf24j40::ie(void) {
+  write_short_ctrl_reg(MRF24J40_INTCON, ~(TXNIE | RXIE | SECIE));
 }
 
-void mrf24j40_pwr_reset(void) {
-  mrf24j40_write_short_ctrl_reg(SOFTRST, RSTPWR);
+void Mrf24j40::pwr_reset(void) {
+  write_short_ctrl_reg(SOFTRST, RSTPWR);
 }
 
-void mrf24j40_bb_reset(void) {
-  mrf24j40_write_short_ctrl_reg(SOFTRST, RSTBB);
+void Mrf24j40::bb_reset(void) {
+  write_short_ctrl_reg(SOFTRST, RSTBB);
 }
 
-void mrf24j40_mac_reset(void) {
-  mrf24j40_write_short_ctrl_reg(SOFTRST, RSTMAC);
+void Mrf24j40::mac_reset(void) {
+  write_short_ctrl_reg(SOFTRST, RSTMAC);
 }
 
-void mrf24j40_rf_reset(void) {
-  uint8_t old = mrf24j40_read_short_ctrl_reg(RFCTL);
-
-  mrf24j40_write_short_ctrl_reg(RFCTL, old | RFRST);
-  mrf24j40_write_short_ctrl_reg(RFCTL, old & ~RFRST);
-  mrf24j40_delay_ms(2);
+void Mrf24j40::rf_reset(void) {
+  uint8_t old = read_short_ctrl_reg(RFCTL);
+  write_short_ctrl_reg(RFCTL, old | RFRST);
+  write_short_ctrl_reg(RFCTL, old & ~RFRST);
+  delay(2);
 }
 
-uint8_t mrf24j40_get_pending_frame(void) {
-  return (mrf24j40_read_short_ctrl_reg(TXNCON) >> 4) & 0x01;
+uint8_t Mrf24j40::get_pending_frame(void) {
+  return (read_short_ctrl_reg(TXNCON) >> 4) & 0x01;
 }
 
-void mrf24j40_rxfifo_flush(void) {
-  mrf24j40_write_short_ctrl_reg(RXFLUSH, (mrf24j40_read_short_ctrl_reg(RXFLUSH) | _RXFLUSH));
+void Mrf24j40::rxfifo_flush(void) {
+  write_short_ctrl_reg(RXFLUSH, (read_short_ctrl_reg(RXFLUSH) | _RXFLUSH));
 }
 
-void mrf24j40_set_channel(int16_t ch) {
-  mrf24j40_write_long_ctrl_reg(RFCON0, CHANNEL(ch) | RFOPT(0x03));
-  mrf24j40_rf_reset();
+void Mrf24j40::set_channel(int16_t ch) {
+  write_long_ctrl_reg(RFCON0, CHANNEL(ch) | RFOPT(0x03));
+  rf_reset();
 }
 
-void mrf24j40_set_promiscuous(bool crc_check) {
+void Mrf24j40::set_promiscuous(bool crc_check) {
   uint8_t w = NOACKRSP;
   if (!crc_check) {
     w |= ERRPKT;
   } else {
     w |= PROMI;
   }
-
-  mrf24j40_write_short_ctrl_reg(RXMCR, w);
+  write_short_ctrl_reg(RXMCR, w);
 }
 
-void mrf24j40_set_coordinator(void) {
-  mrf24j40_write_short_ctrl_reg(RXMCR, mrf24j40_read_short_ctrl_reg(RXMCR) | PANCOORD);
+void Mrf24j40::set_coordinator(void) {
+  write_short_ctrl_reg(RXMCR, read_short_ctrl_reg(RXMCR) | PANCOORD);
 }
 
-void mrf24j40_clear_coordinator(void) {
-  mrf24j40_write_short_ctrl_reg(RXMCR, mrf24j40_read_short_ctrl_reg(RXMCR) & ~PANCOORD);
+void Mrf24j40::clear_coordinator(void) {
+  write_short_ctrl_reg(RXMCR, read_short_ctrl_reg(RXMCR) & ~PANCOORD);
 }
 
-void mrf24j40_set_pan(uint8_t *pan) {
-  mrf24j40_write_short_ctrl_reg(PANIDL, pan[0]);
-  mrf24j40_write_short_ctrl_reg(PANIDH, pan[1]);
+void Mrf24j40::set_pan(uint8_t *pan) {
+  write_short_ctrl_reg(PANIDL, pan[0]);
+  write_short_ctrl_reg(PANIDH, pan[1]);
 }
 
-void mrf24j40_set_short_addr(uint8_t *addr) {
-  mrf24j40_write_short_ctrl_reg(SADRL, addr[0]);
-  mrf24j40_write_short_ctrl_reg(SADRH, addr[1]);
+void Mrf24j40::set_short_addr(uint8_t *addr) {
+  write_short_ctrl_reg(SADRL, addr[0]);
+  write_short_ctrl_reg(SADRH, addr[1]);
 }
 
-void mrf24j40_set_eui(uint8_t *eui) {
-  mrf24j40_write_short_ctrl_reg(EADR0, eui[0]);
-  mrf24j40_write_short_ctrl_reg(EADR1, eui[1]);
-  mrf24j40_write_short_ctrl_reg(EADR2, eui[2]);
-  mrf24j40_write_short_ctrl_reg(EADR3, eui[3]);
-  mrf24j40_write_short_ctrl_reg(EADR4, eui[4]);
-  mrf24j40_write_short_ctrl_reg(EADR5, eui[5]);
-  mrf24j40_write_short_ctrl_reg(EADR6, eui[6]);
-  mrf24j40_write_short_ctrl_reg(EADR7, eui[7]);
+void Mrf24j40::set_eui(uint8_t *eui) {
+  write_short_ctrl_reg(EADR0, eui[0]);
+  write_short_ctrl_reg(EADR1, eui[1]);
+  write_short_ctrl_reg(EADR2, eui[2]);
+  write_short_ctrl_reg(EADR3, eui[3]);
+  write_short_ctrl_reg(EADR4, eui[4]);
+  write_short_ctrl_reg(EADR5, eui[5]);
+  write_short_ctrl_reg(EADR6, eui[6]);
+  write_short_ctrl_reg(EADR7, eui[7]);
 }
 
-void mrf24j40_set_coordinator_short_addr(uint8_t *addr) {
-  mrf24j40_write_short_ctrl_reg(ASSOSADR0, addr[0]);
-  mrf24j40_write_short_ctrl_reg(ASSOSADR1, addr[1]);
+void Mrf24j40::set_coordinator_short_addr(uint8_t *addr) {
+  write_short_ctrl_reg(ASSOSADR0, addr[0]);
+  write_short_ctrl_reg(ASSOSADR1, addr[1]);
 }
 
-void mrf24j40_set_coordinator_eui(uint8_t *eui) {
-  mrf24j40_write_short_ctrl_reg(ASSOEADR0, eui[0]);
-  mrf24j40_write_short_ctrl_reg(ASSOEADR1, eui[1]);
-  mrf24j40_write_short_ctrl_reg(ASSOEADR2, eui[2]);
-  mrf24j40_write_short_ctrl_reg(ASSOEADR3, eui[3]);
-  mrf24j40_write_short_ctrl_reg(ASSOEADR4, eui[4]);
-  mrf24j40_write_short_ctrl_reg(ASSOEADR5, eui[5]);
-  mrf24j40_write_short_ctrl_reg(ASSOEADR6, eui[6]);
-  mrf24j40_write_short_ctrl_reg(ASSOEADR7, eui[7]);
+void Mrf24j40::set_coordinator_eui(uint8_t *eui) {
+  write_short_ctrl_reg(ASSOEADR0, eui[0]);
+  write_short_ctrl_reg(ASSOEADR1, eui[1]);
+  write_short_ctrl_reg(ASSOEADR2, eui[2]);
+  write_short_ctrl_reg(ASSOEADR3, eui[3]);
+  write_short_ctrl_reg(ASSOEADR4, eui[4]);
+  write_short_ctrl_reg(ASSOEADR5, eui[5]);
+  write_short_ctrl_reg(ASSOEADR6, eui[6]);
+  write_short_ctrl_reg(ASSOEADR7, eui[7]);
 }
 
-void mrf24j40_set_key(uint16_t address, uint8_t *key) {
-  mrf24j40_spi_preamble();
-  _mrf24j40_write_long_addr(address, 1);
+void Mrf24j40::set_key(uint16_t address, uint8_t *key) {
+  spi_preamble();
+  _write_long_addr(address, 1);
 
   for (int16_t i = 0; i < 16; i++) {
-    mrf24j40_spi_write(key[i]);
+    spi_write(key[i]);
   }
 
-  mrf24j40_spi_postamble();
+  spi_postamble();
 }
 
-void mrf24j40_hard_reset() {
-  mrf24j40_reset_pin(0);
-
-  mrf24j40_delay_us(192);
-  mrf24j40_reset_pin(1);
-  mrf24j40_delay_us(192);
+void Mrf24j40::hard_reset(void) {
+  _set_reset_pin(0);
+  delayMicroseconds(192);
+  _set_reset_pin(1);
+  delayMicroseconds(192);
 }
 
-void mrf24j40_initialize() {
-  mrf24j40_cs_pin(1);
-  mrf24j40_wake_pin(1);
-
-  mrf24j40_hard_reset();
-
-  mrf24j40_write_short_ctrl_reg(SOFTRST, (RSTPWR | RSTBB | RSTMAC));
-
-  mrf24j40_delay_us(192);
-
-  mrf24j40_write_short_ctrl_reg(PACON2, FIFOEN | TXONTS(0x18));
-  mrf24j40_write_short_ctrl_reg(TXSTBL, RFSTBL(9) | MSIFS(5));
-  mrf24j40_write_long_ctrl_reg(RFCON1, VCOOPT(0x01));
-  mrf24j40_write_long_ctrl_reg(RFCON2, PLLEN);
-  mrf24j40_write_long_ctrl_reg(RFCON6, _20MRECVR);
-  mrf24j40_write_long_ctrl_reg(RFCON7, SLPCLKSEL(0x02));
-  mrf24j40_write_long_ctrl_reg(RFCON8, RFVCO);
-  mrf24j40_write_long_ctrl_reg(SLPCON1, SLPCLKDIV(1) | CLKOUTDIS);
-  mrf24j40_write_short_ctrl_reg(RXFLUSH, (WAKEPAD | WAKEPOL));
-
-  mrf24j40_write_short_ctrl_reg(BBREG2, CCAMODE(0x02) | CCASTH(0x00));
-  mrf24j40_write_short_ctrl_reg(CCAEDTH, 0x60);
-
-  mrf24j40_write_short_ctrl_reg(BBREG6, RSSIMODE2);
-
-  mrf24j40_rxfifo_flush();
-
-  mrf24j40_ie();
+void Mrf24j40::soft_reset(void) {
+  write_short_ctrl_reg(SOFTRST, (RSTPWR | RSTBB | RSTMAC));
+  delayMicroseconds(192);
 }
 
-void mrf24j40_sleep() {
-  mrf24j40_write_short_ctrl_reg(WAKECON, IMMWAKE);
+void Mrf24j40::_configure_pins(void) {
+  pinMode(_cs_pin, OUTPUT);
+  if (_reset_pin >= 0) {
+    pinMode(_reset_pin, OUTPUT);
+  }
+  if (_wake_pin >= 0) {
+    pinMode(_wake_pin, OUTPUT);
+  }
 
-  uint8_t r = mrf24j40_read_short_ctrl_reg(SLPACK);
-  mrf24j40_wake_pin(0);
-
-  mrf24j40_pwr_reset();
-  mrf24j40_write_short_ctrl_reg(SLPACK, r | _SLPACK);
+  _set_cs_pin(1);
+  _set_reset_pin(1);
+  _set_wake_pin(1);
 }
 
-void mrf24j40_wakeup() {
-  mrf24j40_wake_pin(1);
-  mrf24j40_rf_reset();
+void Mrf24j40::initialize(void) {
+  _configure_pins();
+
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE0);
+  SPI.begin();
+
+  hard_reset();
+  delay(2);
+
+  soft_reset();
+
+  write_short_ctrl_reg(PACON2, FIFOEN | TXONTS(0x18));
+  write_short_ctrl_reg(TXSTBL, RFSTBL(9) | MSIFS(5));
+  write_long_ctrl_reg(RFCON1, VCOOPT(0x01));
+  write_long_ctrl_reg(RFCON2, PLLEN);
+  write_long_ctrl_reg(RFCON6, _20MRECVR);
+  write_long_ctrl_reg(RFCON7, SLPCLKSEL(0x02));
+  write_long_ctrl_reg(RFCON8, RFVCO);
+  write_long_ctrl_reg(SLPCON1, SLPCLKDIV(1) | CLKOUTDIS);
+  write_short_ctrl_reg(RXFLUSH, (WAKEPAD | WAKEPOL));
+
+  write_short_ctrl_reg(BBREG2, CCAMODE(0x02) | CCASTH(0x00));
+  write_short_ctrl_reg(CCAEDTH, 0x60);
+
+  write_short_ctrl_reg(BBREG6, RSSIMODE2);
+
+  rxfifo_flush();
+
+  ie();
 }
 
-void mrf24j40_txpkt(uint8_t *frame, int16_t hdr_len, int16_t sec_hdr_len, int16_t payload_len) {
+void Mrf24j40::sleep(void) {
+  write_short_ctrl_reg(WAKECON, IMMWAKE);
+
+  uint8_t r = read_short_ctrl_reg(SLPACK);
+  _set_wake_pin(0);
+
+  pwr_reset();
+  write_short_ctrl_reg(SLPACK, r | _SLPACK);
+}
+
+void Mrf24j40::wakeup(void) {
+  _set_wake_pin(1);
+  rf_reset();
+}
+
+void Mrf24j40::txpkt(uint8_t *frame, int16_t hdr_len, int16_t sec_hdr_len, int16_t payload_len) {
   int16_t frame_len = hdr_len + sec_hdr_len + payload_len;
 
-  uint8_t w = mrf24j40_read_short_ctrl_reg(TXNCON);
+  uint8_t w = read_short_ctrl_reg(TXNCON);
   w &= ~(TXNSECEN);
 
   if (IEEE_802_15_4_HAS_SEC(frame[0])) {
@@ -240,39 +281,39 @@ void mrf24j40_txpkt(uint8_t *frame, int16_t hdr_len, int16_t sec_hdr_len, int16_
     w |= TXNACKREQ;
   }
 
-  mrf24j40_spi_preamble();
-  _mrf24j40_write_long_addr(TXNFIFO, 1);
-  mrf24j40_spi_write(hdr_len);
-  mrf24j40_spi_write(frame_len);
+  spi_preamble();
+  _write_long_addr(TXNFIFO, 1);
+  spi_write(hdr_len);
+  spi_write(frame_len);
 
   while (frame_len-- > 0) {
-    mrf24j40_spi_write(*frame++);
+    spi_write(*frame++);
   }
 
-  mrf24j40_spi_postamble();
+  spi_postamble();
 
-  mrf24j40_write_short_ctrl_reg(TXNCON, w | TXNTRIG);
+  write_short_ctrl_reg(TXNCON, w | TXNTRIG);
 }
 
-void mrf24j40_set_cipher(uint8_t rxcipher, uint8_t txcipher) {
-  mrf24j40_write_short_ctrl_reg(SECCON0, RXCIPHER(rxcipher) | TXNCIPHER(txcipher));
+void Mrf24j40::set_cipher(uint8_t rxcipher, uint8_t txcipher) {
+  write_short_ctrl_reg(SECCON0, RXCIPHER(rxcipher) | TXNCIPHER(txcipher));
 }
 
-bool mrf24j40_rx_sec_fail() {
-  bool rx_sec_fail = (mrf24j40_read_short_ctrl_reg(RXSR) >> 2) & 0x01;
-  mrf24j40_write_short_ctrl_reg(RXSR, 0x00);
+bool Mrf24j40::rx_sec_fail(void) {
+  bool rx_sec_fail = (read_short_ctrl_reg(RXSR) >> 2) & 0x01;
+  write_short_ctrl_reg(RXSR, 0x00);
   return rx_sec_fail;
 }
 
-void mrf24j40_sec_intcb(bool accept) {
-  uint8_t w = mrf24j40_read_short_ctrl_reg(SECCON0);
+void Mrf24j40::sec_intcb(bool accept) {
+  uint8_t w = read_short_ctrl_reg(SECCON0);
 
   w |= accept ? SECSTART : SECIGNORE;
-  mrf24j40_write_short_ctrl_reg(SECCON0, w);
+  write_short_ctrl_reg(SECCON0, w);
 }
 
-int16_t mrf24j40_txpkt_intcb(void) {
-  uint8_t stat = mrf24j40_read_short_ctrl_reg(TXSTAT);
+int16_t Mrf24j40::txpkt_intcb(void) {
+  uint8_t stat = read_short_ctrl_reg(TXSTAT);
   if (stat & TXNSTAT) {
     if (stat & CCAFAIL) {
       return EBUSY;
@@ -284,20 +325,20 @@ int16_t mrf24j40_txpkt_intcb(void) {
   }
 }
 
-int16_t mrf24j40_rxpkt_intcb(uint8_t *buf, uint8_t *plqi, uint8_t *prssi) {
-  mrf24j40_write_short_ctrl_reg(BBREG1, mrf24j40_read_short_ctrl_reg(BBREG1) | RXDECINV);
+int16_t Mrf24j40::rxpkt_intcb(uint8_t *buf, uint8_t *plqi, uint8_t *prssi) {
+  write_short_ctrl_reg(BBREG1, read_short_ctrl_reg(BBREG1) | RXDECINV);
 
-  mrf24j40_spi_preamble();
-  _mrf24j40_write_long_addr(RXFIFO, 0);
+  spi_preamble();
+  _write_long_addr(RXFIFO, 0);
 
-  uint16_t flen = mrf24j40_spi_read();
+  uint16_t flen = spi_read();
 
   for (uint16_t i = 0; i < flen; i++) {
-    *buf++ = mrf24j40_spi_read();
+    *buf++ = spi_read();
   }
 
-  uint8_t lqi = mrf24j40_spi_read();
-  uint8_t rssi = mrf24j40_spi_read();
+  uint8_t lqi = spi_read();
+  uint8_t rssi = spi_read();
 
   if (plqi != NULL) {
     *plqi = lqi;
@@ -307,18 +348,18 @@ int16_t mrf24j40_rxpkt_intcb(uint8_t *buf, uint8_t *plqi, uint8_t *prssi) {
     *prssi = rssi;
   }
 
-  mrf24j40_spi_postamble();
+  spi_postamble();
 
-  mrf24j40_rxfifo_flush();
-  mrf24j40_write_short_ctrl_reg(BBREG1, mrf24j40_read_short_ctrl_reg(BBREG1) & ~RXDECINV);
+  rxfifo_flush();
+  write_short_ctrl_reg(BBREG1, read_short_ctrl_reg(BBREG1) & ~RXDECINV);
 
   return flen;
 }
 
-int16_t mrf24j40_int_tasks(void) {
+int16_t Mrf24j40::int_tasks(void) {
   int16_t ret = 0;
 
-  uint8_t stat = mrf24j40_read_short_ctrl_reg(INTSTAT);
+  uint8_t stat = read_short_ctrl_reg(INTSTAT);
 
   if (stat & RXIF) {
     ret |= MRF24J40_INT_RX;
@@ -335,3 +376,10 @@ int16_t mrf24j40_int_tasks(void) {
   return ret;
 }
 
+void Mrf24j40::spi_write(uint8_t byte) {
+  SPI.transfer(byte);
+}
+
+uint8_t Mrf24j40::spi_read(void) {
+  return SPI.transfer(0xFF);
+}
